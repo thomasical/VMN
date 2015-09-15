d05857c4c42ae825675e5e02f276f1f19a18f12d
Matt Beckett <beck24@gmail.com>
2015-09-15 16:04:03 -0700

    update hybridauth

--- mod/elgg_hybridauth/actions/register.php
+++ mod/elgg_hybridauth/actions/register.php
@@ -60,7 +60,6 @@ if (elgg_get_config('allow_registration')) {
 		if (strcmp($password, $password2) != 0) {
 			throw new RegistrationException(elgg_echo('RegistrationException:PasswordMismatch'));
 		}
-
 		if ($verified) {
 			elgg_unregister_plugin_hook_handler('register', 'user', 'uservalidationbyemail_disable_new_user');
 		}
